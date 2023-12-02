5e68040c25f77ca37c656f769512d7a5687fec04
Buzala21 <soso.buzaladze2007i@gmail.com>
2023-12-02 19:58:29 +0400

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <span className={styles.mySpan}>ეს არის მთავარი გვერდი</span>
+    </main>
+  )
+}
