f5509aeaa679f71748ab1bc709b356965ca766c3
Buzala21 <soso.buzaladze2007i@gmail.com>
2023-12-13 23:38:02 +0400

    Adding Homepage Interface And About Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
-
 import styles from './page.module.css'
+import Image from 'next/image'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <span className={styles.mySpan}>ეს არის მთავარი გვერდი</span>
+    <main className={styles.container}>
+      <div className={styles.item} >
+        <h1 className={styles.title} >Better design  for your digital products</h1>
+        <p className={styles.desc} >Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.</p>
+        <button className={styles.button} >See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image
+        src={Hero} alt='hero' className={styles.img}/>
+
+      </div>
     </main>
   )
 }
