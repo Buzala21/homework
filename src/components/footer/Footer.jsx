41743541fb4ae4e7f136f7bba03ea75bd165cbd9
Buzala21 <soso.buzaladze2007i@gmail.com>
2023-12-17 21:50:33 +0400

    Adding Footer Links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./footer.module.css"
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Footer() {
   return (
@@ -8,10 +9,23 @@ function Footer() {
       {/* ქაარი ქრის */}
       <div>@all rights reserved</div>
       <div className={styles.socialNetwork} >
-        <Image src="/1.png" width={15} className={styles.icon} height={15} alt='facebook'/>
+
+        <Link target='blank' href={"https://www.facebook.com"}>
+        <Image  src="/1.png" width={15} className={styles.icon} height={15} alt='facebook'/>
+        </Link>
+
+        <Link target='blank' href={"https://www.instagram.com"}>
         <Image src="/2.png" width={15} className={styles.icon} height={15} alt='instagram'/>
+        </Link>
+
+        <Link target='blank' href={"https://twitter.com/tvitter"}>
         <Image src="/3.png" width={15} className={styles.icon} height={15} alt='x'/>
-        <Image src="/4.png" width={15} className={styles.icon} height={15} alt='youtube'/>
+        </Link>
+
+        <Link target='blank' href={"https://www.youtube.com"}>
+        <Image src="/4.png" width={15} className={styles.icon} height={15} alt='youtube'/> 
+        </Link>
+
       </div>
     </div>
 
